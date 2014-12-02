6ad7c590a7aac9064a1ab75bdd0b7e4a7382ff3c
Jose Pascoal <jose.pascoal@kwamecorp.com>
2014-12-02 12:41:35 +0000

    - Add All apps button widget

---
+++ src/com/fairphone/fplauncher3/widgets/allappswidget/AllAppsWidget.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2013 Fairphone Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fairphone.fplauncher3.widgets.allappswidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.fairphone.fplauncher3.R;
+import com.fairphone.fplauncher3.widgets.appswitcher.AppSwitcherWidget;
+
+public class AllAppsWidget extends AppWidgetProvider
+{
+    private static final String TAG = AllAppsWidget.class.getSimpleName();
+
+    @Override
+    public void onEnabled(Context context)
+    {
+        Log.d(TAG, "Fairphone - WidgetProvicer Context is " + context);
+    }
+
+    @Override
+    public void onDisabled(Context context)
+    {
+        // Called once the last instance of your widget is removed from the
+        // homescreen
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds)
+    {
+        // Widget instance is removed from the homescreen
+        Log.d(TAG, "onDeleted - " + appWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions)
+    {
+        updateUI(context, appWidgetManager, appWidgetId);
+        // Obtain appropriate widget and update it.
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+
+    private void updateUI(Context context, AppWidgetManager appWidgetManager, int appWidgetId)
+    {
+        int code = 0;
+        // get the widgets
+        RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.fp_all_apps_widget);
+
+        Intent launchIntent = new Intent();
+        launchIntent.setAction(AppSwitcherWidget.ACTION_APP_SWITCHER_LAUNCH_ALL_APPS);
+
+        PendingIntent launchPendingIntent = PendingIntent.getBroadcast(context, code++, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        widget.setOnClickPendingIntent(R.id.all_apps_launcher_widget, launchPendingIntent);
+
+        // update the widget data
+        appWidgetManager.updateAppWidget(appWidgetId, null);
+        appWidgetManager.updateAppWidget(appWidgetId, widget);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * android.appwidget.AppWidgetProvider#onUpdate(android.content.Context,
+     * android.appwidget.AppWidgetManager, int[])
+     * 
+     * OnUpdate ==============================================================
+     * context The Context in which this receiver is running. appWidgetManager A
+     * AppWidgetManager object you can call updateAppWidget(ComponentName,
+     * RemoteViews) on. appWidgetIds The appWidgetIds for which an update is
+     * needed. Note that this may be all of the AppWidget instances for this
+     * provider, or just a subset of them.
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        // Called in response to the ACTION_APPWIDGET_UPDATE broadcast when this
+        // AppWidget provider
+        // is being asked to provide RemoteViews for a set of AppWidgets.
+        // Override this method to implement your own AppWidget functionality.
+
+        // iterate through every instance of this widget
+        // remember that it can have more than one widget of the same type.
+        for (int i = 0; i < appWidgetIds.length; i++)
+        { 
+            Log.d(TAG, "Updating All Apps widget #" + i);
+            updateUI(context, appWidgetManager, appWidgetIds[i]);
+        }
+
+    }
+
+}
