43bb1016ff0577281b84b70219d40c91657c912a
Pierre Rudloff <contact@rudloff.pro>
2018-06-11 20:19:39 +0200

    PreferenceActivity is deprecated, use PreferenceFragment instead

--- src/com/fairphone/fplauncher3/SettingsActivity.java
+++ src/com/fairphone/fplauncher3/SettingsActivity.java
@@ -1,15 +1,18 @@
 package com.fairphone.fplauncher3;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends Activity {
 
     public static final String KEY_PREF_ICON_PACK = "pref_iconPack";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
