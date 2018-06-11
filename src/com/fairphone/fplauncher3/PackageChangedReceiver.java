fdc4aa040ea7353e845a4debf8bd589a1d215ac4
Pierre Rudloff <contact@rudloff.pro>
2018-06-11 20:39:15 +0200

    Detect when the current icon pack is uninstalled

--- src/com/fairphone/fplauncher3/PackageChangedReceiver.java
+++ src/com/fairphone/fplauncher3/PackageChangedReceiver.java
@@ -3,6 +3,8 @@ package com.fairphone.fplauncher3;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class PackageChangedReceiver extends BroadcastReceiver {
     @Override
@@ -17,5 +19,16 @@ public class PackageChangedReceiver extends BroadcastReceiver {
         LauncherAppState.setApplicationContext(context.getApplicationContext());
         LauncherAppState app = LauncherAppState.getInstance();
         WidgetPreviewLoader.removePackageFromDb(app.getWidgetPreviewCacheDb(), packageName);
+
+        // If the current icon pack was deleted, we reload the icons.
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String iconPackName = sharedPref.getString(SettingsActivity.KEY_PREF_ICON_PACK, "");
+        if (packageName.equals(iconPackName)) {
+            IconCache iconCache = LauncherAppState.getInstance().getIconCache();
+
+            iconCache.flush();
+            iconCache.loadIconPack();
+            app.getModel().forceReload();
+        }
     }
 }
